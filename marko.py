5fee15b49809a280c5e67e1b205bd2a010d71a87
MarkGalugalu <76002016+MarkGalugalu@users.noreply.github.com>
2020-12-14 22:14:09 +0300

    Add files via upload

---
+++ marko.py
@@ -0,0 +1,13 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> IMPORT MDP
+SyntaxError: invalid syntax
+>>> def logistic_map(x,r):
+	return r*x*(1-x)
+dforce = N.sin(10*N.pi*t) + N.sin(26*N.pi*t)
+SyntaxError: invalid syntax
+>>> sequence = {mdp.nodes.EtaComputerNode(), mdp.nodes.TimeFramesNode(10)
+	   mdp.nodes.PolynomialExpansionNode(3), mdp.nodes.SFANode(output_dim=10
+								   
+SyntaxError: invalid syntax
+>>> 
